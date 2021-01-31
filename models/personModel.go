662e962bd889ac323f0249f5a4a621a79913c215
Agus Ntoi <ntoiagussaputra@gmail.com>
2021-01-31 23:50:11 +0700

    Revert "refactor(models): add nip on person model"
    This reverts commit 4183e3615389a4b37253a68b72211ccb5bd3119a.

--- models/personModel.go
+++ models/personModel.go
@@ -7,8 +7,8 @@ import (
 // Persons model
 type Persons struct {
 	gorm.Model
-	Nip, FullName, FirstName, LastName, BirthPlace, Gender, ZoneLocation string
-	BirthDate                                                            string
-	SubDistrictID                                                        uint
-	SubDistrict                                                          SubDistricts
+	FullName, FirstName, LastName, BirthPlace, Gender, ZoneLocation string
+	BirthDate                                                       string
+	SubDistrictID                                                   uint
+	SubDistrict                                                     SubDistricts
 }
