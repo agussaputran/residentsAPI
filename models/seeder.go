54d549ecc11fd568ee70fe7a400c918af1ded2cc
Agus Ntoi <ntoiagussaputra@gmail.com>
2021-02-01 11:24:46 +0700

    feat(models): add seeder

--- models/seeder.go
+++ models/seeder.go
@@ -2,6 +2,7 @@ package models
 
 import (
 	"fmt"
+	"strconv"
 
 	"gorm.io/gorm"
 )
@@ -26,17 +27,44 @@ func SeederProvince(db *gorm.DB) {
 }
 
 // SeederDistrict func
-// func SeederDistrict(db *gorm.DB) {
-// 	var districtArr = [...][2]string{
-// 		{"1", "Kota Jogja"},
-// 		{"1", "Bantul"},
-// 		{"1", "Sleman"},
-// 		{"1", "Kulon Progo"},
-// 		{"1", "Gunung Kidul"},
-// 	}
+func SeederDistrict(db *gorm.DB) {
+	var districtArr = [...][2]string{
+		{"2", "Kota Jogja"},
+		{"2", "Bantul"},
+		{"2", "Sleman"},
+		{"1", "Klaten"},
+		{"1", "Magelang"},
+	}
 
-// 	var dist Districts
-// 	for _, v := range districtArr{
-// 		dist.ProvinceID = v
-// 	}
-// }
+	var dist Districts
+	for _, v := range districtArr {
+		data, _ := strconv.ParseUint(v[0], 10, 64)
+		dist.ProvinceID = uint(data)
+		dist.Name = v[1]
+		dist.ID = 0
+		db.Create(&dist)
+	}
+	fmt.Println("Seeder District created")
+}
+
+//SeederSubDistrict func
+func SeederSubDistrict(db *gorm.DB) {
+	var subDistrictArray = [...][2]string{
+		{"1", "Umbulharjo"},
+		{"2", "Banguntapan"},
+		{"3", "Ngaglik"},
+		{"4", "Klaten Selatan"},
+		{"5", "Muntilan"},
+	}
+
+	var subDist SubDistricts
+	for _, v1 := range subDistrictArray {
+		data, _ := strconv.ParseUint(v1[0], 10, 64)
+		subDist.DistrictID = uint(data)
+		subDist.Name = v1[1]
+		subDist.ID = 0
+		db.Create(&subDist)
+
+	}
+	fmt.Println("Seeder Sub District created")
+}
